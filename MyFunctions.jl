970e3eb85dd580b1c0707b21484a9164fbca4d30
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2022-12-08 20:32:58 +0100

    Dodano rozwiązanie zadania 2

--- MyFunctions.jl
+++ MyFunctions.jl
@@ -1,7 +1,7 @@
 #author: Norbert Jaśniewicz
 module MyFunctions
-
-  export ilorazyRoznicowe, warNewton
+  import Plots
+  export ilorazyRoznicowe, warNewton, rysujNnfx
 
   function ilorazyRoznicowe(x::Vector{Float64}, f::Vector{Float64})
     if (length(x) != length(f))
@@ -11,8 +11,7 @@ module MyFunctions
     if length(f) == 1
       return copy(f)
     end
-
-    result :: Vector{Float64} = zeros(length(f))
+    result::Vector{Float64} = zeros(length(f))
     result[1] = f[1]
 
     iteration = 1
@@ -32,6 +31,27 @@ module MyFunctions
   end
 
   function warNewton(x::Vector{Float64}, fx::Vector{Float64}, t::Float64)
+    result::Float64 = last(fx)
+    index = length(fx) - 1
+    while index > 0
+      result = fx[index] + (t - x[index]) * result
+      index -= 1
+      println("siema")
+    end
 
+    return result
+  end
+
+  function rysujNnfx(f, a::Float64, b::Float64, n::Int)
+    h::Float64 = (b - a) / n
+    args::Vector{Float64} = [a + h * i for i in 0:n]
+    fVals::Vector{Float64} = map(f, args)
+    quotients::Vector{Float64} = ilorazyRoznicowe(args, fVals)
+    interVals::Vector{Float64} = map((x) -> warNewton(args, quotients, x))
+    fig = Plots.plot(f, a, b, title="Wykresy", label=["f"])
+    Plots.plot!(args, interVals, label=["Interpolation"])
+    display(fig)
+
+  end
 
 end
---
+++ zad2Test.jl
@@ -0,0 +1,16 @@
+include("MyFunctions.jl")
+using .MyFunctions
+
+
+function main()
+  arg1::Vector{Float64} = [3.0, 1.0, 5.0, 6.0]
+  arg2::Vector{Float64} = [1.0, -3.0, 2.0, 4.0]
+  res = MyFunctions.ilorazyRoznicowe(arg1, arg2)
+  println(MyFunctions.warNewton(arg1, res, arg1[3]))
+  arg1 = [3.0]
+  arg2 = [1.0]
+  res = MyFunctions.ilorazyRoznicowe(arg1, arg2)
+  println(MyFunctions.warNewton(arg1, res, arg1[1]))
+end
+
+main()
