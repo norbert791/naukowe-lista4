79f11f3b7153f8a98160de82dac453de126d392c
norbert <norbert.jasniewicz@protonmail.com>
2022-12-11 19:38:12 +0100

    Zadania 5 i 6 nie wyłączają się od razu

--- zad5.jl
+++ zad5.jl
@@ -6,10 +6,12 @@ using .MyFunctions
 function main()
   for i in [5, 10, 15]
     MyFunctions.rysujNnfx(exp, 0.0, 1.0, i)
+    t = readline()
   end
 
   for i in [5, 10, 15]
-    MyFunctions.rysujNnfx(x -> (x^2)*sin(x), -1.0, 1.0, i,)
+    MyFunctions.rysujNnfx(x -> (x^2)*sin(x), -1.0, 1.0, i)
+    t = readline()
   end
 
 end
--- zad6.jl
+++ zad6.jl
@@ -6,11 +6,14 @@ using .MyFunctions
 function main()
   for i in [5, 10, 15]
     MyFunctions.rysujNnfx(abs, -1.0, 1.0, i)
+    t = readline()
   end
 
   for i in [5, 10, 15]
     MyFunctions.rysujNnfx(x -> 1/(1 + x^2), -5.0, 5.0, i)
+    t = readline()
   end
+  
 end
 
 main()
